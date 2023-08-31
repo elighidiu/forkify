28bfb245c1659f4489a5cd85e1fa3d8db1c38aed
Your Nameeli <elighidiu@yahoo.com>
2023-08-31 12:57:52 +0200

    render spinner

--- src/js/controller.js
+++ src/js/controller.js
@@ -13,10 +13,20 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+const renderSpinner = function (parentEl) {
+  const markup = `<div class="spinner">
+  <svg>
+    <use href="${icons}#icon-loader"></use>
+  </svg>
+</div>`;
+  parentEl.innerHTML = '';
+  parentEl.insertAdjacentHTML('afterbegin', markup);
+};
+
 const showRecipe = async function () {
   try {
     //1) -------------Loading recepies------------
-
+    renderSpinner(recipeContainer);
     //fetch will return a promise. since we are in an async function we can then await for that promise
     const res = await fetch(
       'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bca5d'
