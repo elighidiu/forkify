84928da31d0d6c3400659249481bddb61c46da49
Elisabeta Ghidiu <82021643+elighidiu@users.noreply.github.com>
2023-09-07 10:45:17 +0200

    Add files via upload

--- src/js/model.js
+++ src/js/model.js
@@ -1,14 +1,22 @@
+import { result } from 'lodash-es';
 import { async } from 'regenerator-runtime';
 import { API_URL } from './config';
+import { RES_PER_PAGE } from './config';
 import { getJSON } from './helpers';
 
 export const state = {
   recipe: {},
+  search: {
+    query: '',
+    results: [],
+    page: 1,
+    resultsPerPage: RES_PER_PAGE,
+  },
 };
 
 export const loadRecipe = async function (id) {
   try {
-    const data = await getJSON(`${API_URL}/${id}`);
+    const data = await getJSON(`${API_URL}${id}`);
 
     const { recipe } = data.data;
 
@@ -24,8 +32,44 @@ export const loadRecipe = async function (id) {
     };
 
     console.log(state.recipe);
-  } catch (error) {
-    //temp error handling
-    console.error(`${error}💥💥💥`);
+  } catch (err) {
+    console.error(`${err}💥💥💥`);
+    throw err;
   }
+};
+
+export const loadSearchResults = async function (query) {
+  try {
+    state.search.query = query;
+
+    const data = await getJSON(`${API_URL}?search=${query}`);
+    console.log(data);
+    state.search.results = data.data.recipes.map(rec => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        image: rec.image_url,
+      };
+    });
+  } catch (err) {
+    console.error(`${err}💥💥💥`);
+    throw err;
+  }
+};
+
+export const getSearchResultsPage = function (page = state.search.page) {
+  state.search.page = page;
+  const start = (page - 1) * state.search.resultsPerPage; //0;
+  const end = page * state.search.resultsPerPage; // 9;
+  return state.search.results.slice(start, end);
+};
+
+export const updateServings = function (newServings) {
+  state.recipe.ingredients.forEach(ing => {
+    ing.quantity = (ing.quantity * newServings) / state.recipe.servings;
+    //newQt = oldQt*newServings/oldServings
+  });
+
+  state.recipe.servings = newServings;
 };
